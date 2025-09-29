20d4c5741f64b4f4cef16595d7ace41d0ce5a290
Waleed Mehmood <80235010+Waleed-Mehmood@users.noreply.github.com>
2025-09-29 17:02:10 +0500

    Request Limit size increased

--- backend/server.js
+++ backend/server.js
@@ -16,8 +16,8 @@ const ENV = process.env.NODE_ENV || "development";
 const app = express();
 
 // Middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 app.use(cookieParser());
 
 // Serve static files from /public
